9faed9f3a03157da2e55e807f681662fed90964c
Mem0Real <juniourmimo@gmail.com>
2023-12-31 00:50:26 +0300

    Action icons fixed

--- src/app/components/baseComponents/Card.jsx
+++ src/app/components/baseComponents/Card.jsx
@@ -1,36 +1,74 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { AnimatePresence, motion } from "framer-motion";
 import Action from "../dashComponents/operationsComponents/Action";
 
 const Card = ({ id, name, description }) => {
-	const [isHovered, setIsHovered] = useState(false);
+	const [menu, setMenu] = useState({ id: "", state: false });
+	const [isMobile, setIsMobile] = useState(false);
 
-	const handleMouseEnter = () => {
-		setIsHovered(true);
+	// Check if device is desktop or mobile
+	useEffect(() => {
+		const handleResize = () => {
+			setIsMobile(global.window && window.innerWidth < 768);
+		};
+
+		window.addEventListener("resize", handleResize);
+
+		return () => {
+			window.removeEventListener("resize", handleResize);
+		};
+	}, []);
+
+	const handleMouseEnter = (id, isMobile) => {
+		if (!isMobile) {
+			if (menu.id === id) {
+				setMenu({ id: id, state: !menu.state });
+			} else {
+				setMenu({ id: id, state: true });
+			}
+		}
 	};
 
-	const handleMouseLeave = () => {
-		setIsHovered(false);
+	const handleMouseLeave = (id, isMobile) => {
+		if (!isMobile) {
+			if (menu.id === id) {
+				setMenu({ id: id, state: false });
+			}
+		}
+	};
+
+	const handleTap = (id) => {
+		if (menu.id === id) {
+			setMenu({ id: id, state: !menu.state });
+		} else {
+			setMenu({ id: id, state: true });
+		}
 	};
 
 	return (
 		<motion.div
-			className="relative w-[60%] mx-auto border border-white rounded-xl shadow-md shadow-blue-400/80 p-8 pe-24 flex flex-col justify-between items-start gap-4 cursor-pointer hover:bg-neutral-800/70"
+			className="relative w-[60%] mx-auto border border-white rounded-xl shadow-md shadow-blue-400/80 p-8 pe-16 md:pe-24 flex flex-col justify-between items-start gap-4 cursor-pointer hover:bg-neutral-800/70"
 			key={id}
-			onHoverStart={handleMouseEnter}
-			onHoverEnd={handleMouseLeave}
+			onHoverStart={() => handleMouseEnter(id, isMobile)}
+			onHoverEnd={() => handleMouseLeave(id, isMobile)}
 		>
-			<h1 className="text-2xl font-bold underline">{name}</h1>
-			<h1>{description}</h1>
+			<motion.div onTap={() => handleTap(id)}>
+				<h1 className="text-2xl font-bold underline">{name}</h1>
+				<h1>{description}</h1>
+			</motion.div>
 			<AnimatePresence>
 				<motion.div
 					initial={{ opacity: 0, x: 10 }}
-					animate={isHovered ? { opacity: 1, x: 0 } : { opacity: 0, x: 10 }}
+					animate={
+						menu.id === id && menu.state
+							? { opacity: 1, x: 0 }
+							: { opacity: 0, x: 10 }
+					}
 					exit={{ opacity: 0, x: 10 }}
 					transition={{ duration: 0.3 }}
-					className="absolute top-1/2 right-4 flex gap-4"
+					className="absolute top-0 right-4 h-full"
 				>
-					<Action />
+					<Action id={id} />
 				</motion.div>
 			</AnimatePresence>
 		</motion.div>
--- src/app/components/dashComponents/operationsComponents/Action.jsx
+++ src/app/components/dashComponents/operationsComponents/Action.jsx
@@ -1,7 +1,34 @@
 import React from "react";
+import FramerButton from "../../baseComponents/FramerButton";
 
-const Action = () => {
-	return <div>Action</div>;
+import { CiEdit } from "react-icons/ci";
+import { MdDeleteOutline } from "react-icons/md";
+
+const Action = ({ id }) => {
+	const handleEditClick = async () => {
+		console.log("edit clicked");
+		console.log(id);
+	};
+	const handleDeleteClick = async () => {
+		console.log("Delete clicked");
+		console.log(id);
+	};
+	return (
+		<div
+			className={`h-full flex flex-col justify-center gap-16 md:gap-12 items-end md:px-4 py-5`}
+		>
+			<FramerButton
+				text={<CiEdit size={24} color="#0E86D4" />}
+				icon
+				handleClick={handleEditClick}
+			/>
+			<FramerButton
+				text={<MdDeleteOutline size={24} color="#f44" />}
+				icon
+				handleClick={handleDeleteClick}
+			/>
+		</div>
+	);
 };
 
 export default Action;
