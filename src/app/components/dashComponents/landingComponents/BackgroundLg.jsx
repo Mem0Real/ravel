19657486bdd8831f39862b57644def862ac7ff43
Mem0Real <juniourmimo@gmail.com>
2023-12-30 16:50:26 +0300

    Dashboard landing finito

---
+++ src/app/components/dashComponents/landingComponents/BackgroundLg.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Image from "next/image";
+
+const BackgroundLg = () => {
+	return (
+		<div className="hidden md:block basis-1/3 relative w-[20vw] h-[25vh] mt-10 md:-mt-2 -ml-12">
+			<Image
+				fill
+				sizes="(max-width: 768px) 100vw, (max-width: 1200px)"
+				className="object-contain object-left"
+				src="/images/dashBg.png"
+				alt="Assist"
+				priority
+			/>
+		</div>
+	);
+};
+
+export default BackgroundLg;
---
+++ src/app/components/dashComponents/landingComponents/BackgroundSm.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Image from "next/image";
+
+const BackgroundSm = () => {
+	return (
+		<div className="md:hidden relative w-[100vw] h-[20vh]">
+			<Image
+				fill
+				sizes="(max-width: 768px) 100vw, (max-width: 1200px)"
+				className="object-contain object-center"
+				src="/images/dashBgSm.png"
+				alt="Assist"
+				priority
+			/>
+		</div>
+	);
+};
+
+export default BackgroundSm;
